682b7fd006b217e91baef4e3a6e6a7f0a5f0d1a8
jmcaballerofdez <jmcaballerofdez@gmail.com>
2026-06-19 16:47:41 +0200

    Add files via upload
    Versión Final

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 3000,
+  },
+  optimizeDeps: {
+    exclude: ['firebase/app', 'firebase/firestore', 'firebase/auth']
+  }
+})
