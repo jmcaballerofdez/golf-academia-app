9e331684ae6731b215d5540160d95cec39fe5746
jmcaballerofdez <jmcaballerofdez@gmail.com>
2026-06-20 08:58:33 +0200

    Base para GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/golf-academia-app/',
   build: {
     outDir: 'dist',
     chunkSizeWarningLimit: 3000,
